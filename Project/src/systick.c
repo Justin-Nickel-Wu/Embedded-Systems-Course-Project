4265a77957149d45b84982d94fd86046cb73b339
Just1nWu <1295360720@qq.com>
2025-12-22 11:18:44 +0800

    dev: 梳理了UART帧相关逻辑。

--- Project/src/systick.c
+++ Project/src/systick.c
@@ -1,37 +1,31 @@
-//systick.c
-#include "stm32f10x.h"                  // Device header
+// systick.c
+#include "stm32f10x.h" // Device header
 #include "stm32f10x_dma.h"
 
 int TimeSecond = 0;
-int Time1msConunt = 0  ;
+int Time1msConunt = 0;
 
-extern u16 FrameFlag ;
+extern u16 FrameFlag;
 extern u16 RecvTimeOver;
-void time_handle()
-{
-		if(RecvTimeOver > 0 ) //´®¿ÚÖ¡½ÓÊÜ³¬Ê±´¦Àí
-		{
-			RecvTimeOver -- ;
-			if (RecvTimeOver == 0)
-			{
-					FrameFlag = 1;
-			}
-		}
-		
-		
-		Time1msConunt ++ ;	
-		if ( Time1msConunt >= 1000)
-		{ 
-			Time1msConunt -= 1000;
-			TimeSecond ++ ;
-		}
+void time_handle() {
+    if (RecvTimeOver > 0) // ä¸²å£å¸§æŽ¥å—è¶…æ—¶å¤„ç†
+    {
+        RecvTimeOver--;
+        if (RecvTimeOver == 0) {
+            FrameFlag = 1;
+        }
+    }
+
+    Time1msConunt++;
+    if (Time1msConunt >= 1000) {
+        Time1msConunt -= 1000;
+        TimeSecond++;
+    }
 }
 
-
-void systick_init(void)
-{
+void systick_init(void) {
     RCC_ClocksTypeDef RCC_Clocks;
-	
+
     /* SystTick configuration: an interrupt every 1ms */
     RCC_GetClocksFreq(&RCC_Clocks);
     SysTick_Config(RCC_Clocks.HCLK_Frequency / 1000);
@@ -39,10 +33,8 @@ void systick_init(void)
     SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK);
 }
 
-volatile int time1ms ;
-void SysTick_Handler(void)
-{
-  /* Update the LocalTime by adding SYSTEMTICK_PERIOD_MS each SysTick interrupt */
-	time_handle();
- 
+volatile int time1ms;
+void SysTick_Handler(void) {
+    /* Update the LocalTime by adding SYSTEMTICK_PERIOD_MS each SysTick interrupt */
+    time_handle();
 }
